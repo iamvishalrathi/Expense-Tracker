b66170b7511a09c640bbbfef25c542cbacef4913
iamvishalrathi <rajatrathi029@gmail.com>
2025-02-01 18:59:06 +0530

    Improving ui

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,31 +1,26 @@
-import {Card, CardHeader , CardContent,Box ,Typography , Grid , Divider} from "@mui/material"
-
-import Form from "./Form/Form"
-import List from "./List/List"
-
-import { useContext } from "react"
-
+import { Card, CardHeader, CardContent, Box, Typography, Grid, Divider } from "@mui/material";
+import Form from "./Form/Form";
+import List from "./List/List";
+import { useContext } from "react";
 import Add_on from "../add_on/Add_on";
-
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { ExpenseTrackerContext } from "../../context/context";
+import "./style.css";
 
-import { ExpenseTrackerContext } from "../../context/context"
-import "./style.css"
-
-
-export default function Main(){
-
-    const {balance} = useContext(ExpenseTrackerContext)
+export default function Main() {
+    const { balance } = useContext(ExpenseTrackerContext);
 
     return (
-        <Card >
-            <CardHeader align="center" title="Budget Buddy" subheader="Your Path to Financial Clarity" sx={{padding:'10px 0 0 0'}} />
-            
+        <Card sx={{ padding: '20px', margin: '20px', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+            <CardHeader align="center" title="Budget Buddy" subheader="Your Path to Financial Clarity" sx={{ padding: '10px 0 0 0' }} />
             <CardContent>
-            <Typography align="center" variant="h5" mb="10px" >Total Balance ₹{balance} {balance!=0 ? balance>0 ? <ArrowUpwardIcon color="primary" fontSize="large" /> : <ArrowDownwardIcon color="error" fontSize="large" /> : <></>} </Typography>
-            
-            <div className="feature"><Add_on sx={{}}></Add_on></div>
+                <Typography align="center" variant="h5" mb="10px">
+                    Total Balance ₹{balance} {balance !== 0 ? balance > 0 ? <ArrowUpwardIcon color="primary" fontSize="large" /> : <ArrowDownwardIcon color="error" fontSize="large" /> : <></>}
+                </Typography>
+                <Box display="flex" justifyContent="center" alignItems="center" mb="20px">
+                    <Add_on />
+                </Box>
                 <Divider className="divider" />
                 <Form />
             </CardContent>
@@ -37,5 +32,5 @@ export default function Main(){
                 </Grid>
             </CardContent>
         </Card>
-    )
+    );
 }
