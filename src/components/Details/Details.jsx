eab55bb07dd9a28e750ac93ca352d7f8f3a5b186
iamvishalrathi <rajatrathi029@gmail.com>
2025-02-01 19:14:05 +0530

    Ui enhanced

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,12 +1,7 @@
-import {Card , CardHeader , CardContent , Typography} from "@mui/material"
-
-import {Chart as ChartJS, ArcElement , Tooltip , Legend} from 'chart.js';
-
-
+import { Card, CardHeader, CardContent, Typography } from "@mui/material"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from "react-chartjs-2"
-
 import useTransactions from "../../useTransactions"
-
 import "./style.css"
 
 ChartJS.register(
@@ -15,18 +10,18 @@ ChartJS.register(
     Legend
 );
 
-export default function Details({title}){
+export default function Details({ title }) {
 
-    const {total , chartData} = useTransactions(title);
+    const { total, chartData } = useTransactions(title);
     return (
-        <Card className={title=="Income"?"income":"expense"}>
+        <Card sx={{ padding: '20px', margin: '20px', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }} className={title == "Income" ? "income" : "expense"}>
             <CardHeader title={title} />
             <CardContent>
                 <Typography variant="h5" component="div">₹{total}</Typography>
-                {total>0 && <Doughnut data={chartData} />}
-                
+                {total > 0 && <Doughnut data={chartData} />}
+
             </CardContent>
-            
+
         </Card>
     )
 }
